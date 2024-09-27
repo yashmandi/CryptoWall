df42ec0026528f30d07cd3d18f3f21cafdd6288f
yashmandi <yashmandi18@gmail.com>
2024-09-27 08:56:02 +0530

    dropdown

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,41 @@
 import { useState } from "react";
 import { Select } from "antd";
-import logo from "./assets/image.png"
+import logo from "./assets/image1.png";
 
 function App() {
+  const [selectedChain, setSelectedChain] = useState("0x1");
   return (
-    <>
-      <div className="bg-slate-600 h-[600px] w-[350px] flex flex-start flex-col justify-center items-center text-white font-bold text-2xl"></div>
-    </>
+    <div className="bg-[#1B1B1B] h-[600px] w-[350px] text-white font-bold text-2xl">
+      <header className="flex justify-between items-center p-4 bg-white rounded-b-md border-2 shadow-md shadow-gray-700">
+        {/* Logo on the left */}
+        <img src={logo} alt="logo" className="h-[60px] pr-5" />
+
+        {/* Dropdown on the right */}
+        <Select
+          value={selectedChain}
+          onChange={setSelectedChain}
+          options={[
+            {
+              label: "Ethereum",
+              value: "0x10",
+            },
+            {
+              label: "Mumbai Testnet",
+              value: "0x13881",
+            },
+            {
+              label: "Polygon",
+              value: "0x89",
+            },
+            {
+              label: "Avalanche",
+              value: "0xa86a",
+            },
+          ]}
+          className="w-[150px]" // Adjusted width for the dropdown
+        />
+      </header>
+    </div>
   );
 }
 
