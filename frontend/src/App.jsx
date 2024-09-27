e2e58efb7bfdf8adae1c84f3b4710f4f81b93c79
yashmandi <yashmandi18@gmail.com>
2024-09-27 08:03:35 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
+import { Select } from "antd";
+import logo from "./assets/image.png"
 
 function App() {
   return (
     <>
-    <p className="text-3xl font-bold underline">Hello</p>
+      <div className="bg-slate-600 h-[600px] w-[350px] flex flex-start flex-col justify-center items-center text-white font-bold text-2xl"></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
