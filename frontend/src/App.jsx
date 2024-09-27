73c98be820e75883c57ea2a8c5ecf15a6f87a64a
yashmandi <yashmandi18@gmail.com>
2024-09-27 09:27:05 +0530

    ui, footer, buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { Select } from "antd";
 import logo from "./assets/image1.png";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   const [selectedChain, setSelectedChain] = useState("0x1");
   return (
-    <div className="bg-[#1B1B1B] h-[600px] w-[360px] text-white font-bold text-2xl">
+    <div className="bg-[#1B1B1B] h-[600px] w-[360px] text-white font-bold text-2xl rounded-lg">
       <header className="flex justify-between items-center pr-4 p-2 bg-white rounded-b-md border-2 shadow-md shadow-gray-700">
         {/* Logo on the left */}
         <img src={logo} alt="logo" className="h-[60px] w-[220px] pr-5" />
@@ -35,6 +37,9 @@ function App() {
           className="w-[150px]" // Adjusted width for the dropdown
         />
       </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Button } from "antd";
+import logo from "../assets/image.png";
+
+const Home = () => {
+  return (
+    <div>
+      <div className="flex justify-center items-center">
+        <img src={logo} alt="HomepageLogo" className="w-[130px] mt-3 mb-2   " />
+      </div>
+      <div className="flex flex-col items-center justify-center ">
+        <p className="text-[22px]">Welcome to CryptoWall!</p>
+        <p className="text-[14px]">
+          Your first step towards securing your crypto.
+        </p>
+      </div>
+
+      <div className="flex flex-col items-center justify-center mt-12">
+        <h4 className="text-[16px] bg-white text-gray-800 px-3 cursor-pointer hover:bg-[#1B1B1B] border-white border-2 hover:text-white rounded-md">
+          Create a Wallet
+        </h4>
+        <h4 className="text-[15px] mt-4 bg-white text-gray-800 px-2.5 cursor-pointer hover:bg-[#1B1B1B] border-white border-2 hover:text-white rounded-md">
+          Sign in with Seed Phrase
+        </h4>
+      </div>
+
+      <div class="flex justify-center items-end h-[160px]">
+        <p className="text-xs font-normal">
+          Developed by <a href="https://github.com/yashmandi" className="hover:underline font-semibold">@yashmandi</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
