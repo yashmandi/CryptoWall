42939d0eb2eeed839056429e8fc0fa798c0c77db
yashmandi <yashmandi18@gmail.com>
2024-09-29 02:16:14 +0530

    backned setup

--- backend/index.js
+++ backend/index.js
@@ -9,8 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/getTokens", async(req, res)> {
-    
-    return res.status(200).json({});
+    return res.status(200).json
 })
 
 Moralis.start({
