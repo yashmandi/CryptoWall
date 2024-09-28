6b6a41ef0c20aa70bf49d2f5a090f3180b3962bc
yashmandi <yashmandi18@gmail.com>
2024-09-28 12:59:14 +0530

    tabs

--- frontend/src/components/WalletView.jsx
+++ frontend/src/components/WalletView.jsx
@@ -1,17 +1,7 @@
 import React, { useEffect, useState } from "react";
-import {
-  Divider,
-  Tooltip,
-  List,
-  Avatar,
-  Spin,
-  Tabs,
-  Input,
-  Button,
-} from "antd";
-import { LogoutOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router-dom";
 import { FiLogOut } from "react-icons/fi";
+import { Tabs, Tooltip } from "antd";
 
 const tokens = [
   {
@@ -46,6 +36,24 @@ const nfts = [
   "https://nft-preview-media.s3.us-east-1.amazonaws.com/evm/0x1/0x749f5ddf5ab4c1f26f74560a78300563c34b417d/0x90cae88ffc909feab8e4df76abd0652dee98b7bffab29597d898260d91c20aa1/high.jpeg",
 ];
 
+const items = [
+  {
+    key: "3",
+    label: <span className="text-white font-bold">Tokens</span>,
+    children: <>Tokens</>,
+  },
+  {
+    key: "4",
+    label: <span className="text-white font-bold">NFTs</span>,
+    children: <>NFTs</>,
+  },
+  {
+    key: "5",
+    label: <span className="text-white font-bold">Transactions</span>,
+    children: <>Transactions</>,
+  },
+];
+
 function WalletView({
   wallet,
   setWallet,
@@ -67,9 +75,24 @@ function WalletView({
         <div onClick={logout}>
           <FiLogOut className="text-[24px] cursor-pointer" />
         </div>
-        <p className="text-[12px] whitespace-pre-wrap text-center items-center font-normal">
-          {wallet}
-        </p>
+        <div className="flex flex-col">
+          <div className="text-[24px] whitespace-pre-wrap text-center items-center font-semibold">
+            My Wallet
+          </div>
+          <Tooltip
+            title={wallet}
+            className="cursor-pointer mt-1 text-center items-center font-medium text-[16px]"
+          >
+            {wallet.slice(0, 4)}...{wallet.slice(38)}
+          </Tooltip>
+        </div>
+        <hr className="my-4 border-gray-600" />
+        <Tabs
+          defaultActiveKey="1"
+          items={items}
+          className="w-full text-center items-center"
+          style={{ color: "white" }}
+        />
       </div>
     </>
   );
