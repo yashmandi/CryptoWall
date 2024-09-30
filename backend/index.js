75033fb1d04bb70e925f1236c81b2586ae089500
yashmandi <yashmandi18@gmail.com>
2024-09-30 10:12:44 +0530

    updated backend index.js

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,37 @@
-const express = require('express');
-const Moralis = require('moralis').default;
+const express = require("express");
+const Moralis = require("moralis").default;
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
+const cors = require("cors");
+require("dotenv").config();
 const port = 3001;
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/getTokens", async (req, res) => {
-    const { userAddress, chain } = req.query;
+  const { userAddress, chain } = req.query;
+  const tokens = await Moralis.EvmApi.token.getWalletTokenBalances({
+    chain: chain,
+    address: userAddress
+  });
+  const nfts = await Moralis.EvmApi.nft.getWalletNFTs({
+    chain: chain,
+    address: userAddress,
+    mediaItems: true,
+  })
 
-    if (!userAddress || !chain) {
-        return res.status(400).json({ error: "Missing userAddress or chain parameter" });
-    }
+  console.log(nfts.raw.result);
 
-    try {
-        const tokens = await Moralis.EvmApi.token.getWalletTokenBalances({
-            chain: chain,
-            address: userAddress,
-        });
+  const jsonResponse = {
+    tokens: tokens.raw,
+  }
 
-        const jsonResponse = {
-            tokens: tokens.raw
-        };
-
-        return res.status(200).json(jsonResponse);
-    } catch (error) {
-        return res.status(500).json({ error: error.message });
-    }
 });
 
 Moralis.start({
-    apiKey: process.env.MORALIS_KEY,
+  apiKey: process.env.MORALIS_KEY,
 }).then(() => {
-    app.listen(port, () => {
-        console.log("Listening on port: " + port);
-    });
+  app.listen(port, () => {
+    console.log("Listening on port: " + port);
+  });
 });
